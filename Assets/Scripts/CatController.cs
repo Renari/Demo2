a323ef31bc016b2f9f8edf2846a2fe142ce80ef8
Renari <renari@arimil.com>
2016-09-30 21:39:20 -0400

    add sound

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class CatController : MonoBehaviour {
 
+    public AudioClip walk;
+    public AudioClip run;
+    public AudioClip die;
+
+
     public const string MOVEMENT_KEY = "Movement";
     public const string DEAD_KEY = "Dead";
     private Animator animator;
+    private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -22,10 +29,12 @@ public class CatController : MonoBehaviour {
             if (animator.GetInteger(MOVEMENT_KEY) == 0)
             {
                 animator.SetInteger(MOVEMENT_KEY, 1);
+                playSound(walk);
             }
             else if (animator.GetInteger(MOVEMENT_KEY) > 0)
             {
                 animator.SetInteger(MOVEMENT_KEY, 2);
+                playSound(run);
             }
             else
             {
@@ -38,10 +47,12 @@ public class CatController : MonoBehaviour {
             if (animator.GetInteger(MOVEMENT_KEY) == 0)
             {
                 animator.SetInteger(MOVEMENT_KEY, -1);
+                playSound(walk);
             }
             else if (animator.GetInteger(MOVEMENT_KEY) < 0)
             {
                 animator.SetInteger(MOVEMENT_KEY, -2);
+                playSound(run);
             }
             else
             {
@@ -51,24 +62,44 @@ public class CatController : MonoBehaviour {
         }
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (animator.GetInteger(MOVEMENT_KEY) > 0)
+            if (animator.GetInteger(MOVEMENT_KEY) == 2)
             {
                 animator.SetInteger(MOVEMENT_KEY, animator.GetInteger(MOVEMENT_KEY) - 1);
+                playSound(walk);
             }
-            else if (animator.GetInteger(MOVEMENT_KEY) < 0)
+            else if (animator.GetInteger(MOVEMENT_KEY) == 1)
+            {
+                animator.SetInteger(MOVEMENT_KEY, animator.GetInteger(MOVEMENT_KEY) - 1);
+                playSound(null);
+            }
+            else if (animator.GetInteger(MOVEMENT_KEY) == -1)
             {
                 animator.SetInteger(MOVEMENT_KEY, animator.GetInteger(MOVEMENT_KEY) + 1);
+                playSound(null);
+            }
+            else if (animator.GetInteger(MOVEMENT_KEY) == -1)
+            {
+                animator.SetInteger(MOVEMENT_KEY, animator.GetInteger(MOVEMENT_KEY) + 1);
+                playSound(walk);
             }
         }
         else if (Input.GetKeyDown(KeyCode.Backspace))
         {
             animator.SetBool(DEAD_KEY, true);
             animator.SetInteger(MOVEMENT_KEY, 0);
+            playSound(die);
+            audioSource.loop = false;
         }
     }
     
     private void invertDirection()
     {
         animator.SetInteger(MOVEMENT_KEY, animator.GetInteger(MOVEMENT_KEY) * -1);
+    }
+    
+    private void playSound(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
     }
 }
